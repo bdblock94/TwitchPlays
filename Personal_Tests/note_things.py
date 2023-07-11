c2a5b26e26fa597d3da046e06411ae6f2886ccd1
bdblock94 <bdblock94@gmail.com>
2023-07-11 00:14:15 -0500

    Just setting up my own test files

---
+++ Personal_Tests/note_things.py
@@ -0,0 +1,39 @@
+
+#Prints "this is a test string 1 2 3"
+a = "1" 
+b = "2"
+c = "3"
+
+my_list = ['A string',23,100.232,'o']  #List example
+
+print("this is a test string {} {} {}".format(a,b,c))
+
+
+#Prints "THIS is a test even testier"
+d="this "
+e="is a test"
+
+print(d.upper() + e + " even testier")
+
+# Prints "23"
+print(my_list[1])
+
+
+#Prints 'A'
+rty = e[3]
+rty.upper()
+
+
+#Dictionary example | Prints "VALUE1"
+my_dict = {'key1':'value1','key2':'value2'}
+
+my_dict["key1"].upper()
+
+
+#Prints "a value1"
+print("a " + my_dict["key1"])
+
+
+#example of a set 
+set([1,1,2,3])
+
